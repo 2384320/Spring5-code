a5d4bf27550e2d271a80eb448a248eabf628628c
2384320 <chdms1549@gmail.com>
2023-04-04 00:11:47 +0900

    chap05 공부 끝

---
+++ sp5-chap05/src/main/java/config/AppCtx.java
@@ -0,0 +1,30 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import spring.*;
+
+@Configuration
+@ComponentScan(basePackages = {"spring"})
+public class AppCtx {
+    @Bean
+    @Qualifier("printer")
+    public MemberPrinter memberPrinter1() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    @Qualifier("summaryPrinter")
+    public MemberSummaryPrinter memberPrinter2() { return new MemberSummaryPrinter(); }
+
+    @Bean
+    public VersionPrinter versionPrinter() {
+        VersionPrinter versionPrinter = new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+        return versionPrinter;
+    }
+}
