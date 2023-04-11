051c9e56dc152fc765122aaea477c7bcf66a3752
2384320 <chdms1549@gmail.com>
2023-04-11 22:59:32 +0900

    ch08

---
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppCtx {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+        ds.setUsername("spring5");
+        ds.setPassword("spring5");
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true); //유휴 커넥션 검사
+        ds.setMinEvictableIdleTimeMillis(100 * 60 * 3); //최소 유휴 시간 3분
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 10); //10초 주기
+        return ds;
+    }
+}
---
+++ sp5-chap08/src/main/java/spring/MemberDao.java
@@ -0,0 +1,21 @@
+package spring;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberDao {
+    public Member selectByEmail(String email) {
+        return null;
+    }
+
+    public void insert(Member member) {
+    }
+
+    public void update(Member member) {
+    }
+
+    public Collection<Member> selectAll() {
+        return null;
+    }
+}
