8afaa0dc9d90bf6c754053f5fcb2c75cd51617d9
2384320 <chdms1549@gmail.com>
2023-04-14 00:09:08 +0900

    ch08 공부끝

--- sp5-chap08/src/main/java/config/AppCtx.java
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -3,9 +3,19 @@ package config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import spring.ChangePasswordService;
 import spring.MemberDao;
+import spring.MemberInfoPrinter;
+import spring.MemberListPrinter;
+import spring.MemberPrinter;
+import spring.MemberRegisterService;
 
 @Configuration
+@EnableTransactionManagement
 public class AppCtx {
 
     @Bean(destroyMethod = "close")
@@ -17,14 +27,51 @@ public class AppCtx {
         ds.setPassword("spring5");
         ds.setInitialSize(2);
         ds.setMaxActive(10);
-        ds.setTestWhileIdle(true); //유휴 커넥션 검사
-        ds.setMinEvictableIdleTimeMillis(100 * 60 * 3); //최소 유휴 시간 3분
-        ds.setTimeBetweenEvictionRunsMillis(1000 * 10); //10초 주기
+        ds.setTestWhileIdle(true);
+        ds.setMinEvictableIdleTimeMillis(60000 * 3);
+        ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
         return ds;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSourceTransactionManager tm = new DataSourceTransactionManager();
+        tm.setDataSource(dataSource());
+        return tm;
     }
 
     @Bean
     public MemberDao memberDao() {
         return new MemberDao(dataSource());
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc() {
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc() {
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao());
+        return pwdSvc;
+    }
+
+    @Bean
+    public MemberPrinter memberPrinter() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    public MemberListPrinter listPrinter() {
+        return new MemberListPrinter(memberDao(), memberPrinter());
+    }
+
+    @Bean
+    public MemberInfoPrinter infoPrinter() {
+        MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+        infoPrinter.setMemberDao(memberDao());
+        infoPrinter.setPrinter(memberPrinter());
+        return infoPrinter;
     }
 }
---
+++ sp5-chap08/src/main/java/main/MainForCPS.java
@@ -0,0 +1,25 @@
+package main;
+
+import config.AppCtx;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.ChangePasswordService;
+import spring.MemberNotFoundException;
+import spring.WrongIdPasswordException;
+
+public class MainForCPS {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+        ChangePasswordService cps = ctx.getBean("changePwdSvc", ChangePasswordService.class);
+        try {
+            cps.changePassword("madvirus@madvirus.net", "1234", "1111");
+            System.out.println("암호 변경");
+        } catch (MemberNotFoundException e) {
+            System.out.println("회원 데이터가 존재하지 않음");
+        } catch (WrongIdPasswordException e) {
+            System.out.println("암호가 올바르지 않음");
+        }
+
+        ctx.close();
+    }
+}
